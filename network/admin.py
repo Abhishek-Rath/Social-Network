45b1274e3c7e6cf2e2149a2f1e2617be464f4150
Abhishek-Rath <abhi.rath39@gmail.com>
2022-01-05 14:20:00 +0530

    registered modela in django admin

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Follow, Post
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Follow)
+
